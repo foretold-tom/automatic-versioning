2853a0bdaafd4edff2a5cf0615e9bc9899f9dfda
Tom <70920899+foretold-tom@users.noreply.github.com>
2020-11-15 15:08:04 -0700

    Implement automatic versioning in SimpleNetFxLibrary project.

--- src/SimpleNetFxLibrary/SimpleNetFxLibrary/Properties/AssemblyInfo.cs
+++ src/SimpleNetFxLibrary/SimpleNetFxLibrary/Properties/AssemblyInfo.cs
@@ -22,15 +22,6 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("1c58282d-f73e-41e3-a135-0fc245dea916")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// These values are no longer needed here.
+//    [assembly: AssemblyVersion("1.0.0.0")]
+//    [assembly: AssemblyFileVersion("1.0.0.0")]
